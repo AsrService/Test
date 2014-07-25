191d920973f33d3a04bf5efdc515263200e273b1
allu12 <allu12@gmail.com>
2014-07-25 15:21:25 +0530

    22
    2222

--- Sample/src/main/java/com/sample/controller/HomeController.java
+++ Sample/src/main/java/com/sample/controller/HomeController.java
@@ -17,6 +17,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		System.out.println(1);
 		 System.out.println(2);
+			System.out.println(1);
 		return "home";
 	}
 	
