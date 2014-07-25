5557877bb70ebdd639dcbe1e0103d0f9f0fd70c8
allu12 <allu12@gmail.com>
2014-07-25 15:45:26 +0530

    aaaa

--- Sample/src/main/java/com/sample/controller/HomeController.java
+++ Sample/src/main/java/com/sample/controller/HomeController.java
@@ -16,6 +16,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		System.out.println(1);
+		System.out.println(1);
 		return "home";
 	}
 	
